4ebb2fb33ecd04b2eb015fdade89cfe8bf208190
Matthew Petriella <petriellaproductions@gmail.com>
2022-03-17 15:14:02 -0400

    first card created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-feather.replace();
+//future icon use with ajax
+//feather.replace();
+
+//expanding flex cards
+//$(".option").click(function(){
+   //$(".option").removeClass("active");
+    //$(this).addClass("active");
+    
+ //});
+  
+ 
